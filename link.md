617a3479a262e8d0f651d52e4534d5ec36c1d5ff
ObNitram <mart1.guillemot@gmail.com>
2024-09-16 19:26:49 +0200

    format the link page to be in alphabetical order

--- link.md
+++ link.md
@@ -1,3 +1,13 @@
+## Calendrier des cours de M1 RES
+
+### ARES
+https://obnitram.github.io/sorbonne-calendar/m1/res/ares/m1-res-ares-group1.ics
+
+https://obnitram.github.io/sorbonne-calendar/m1/res/ares/m1-res-ares-group3.ics
+
+https://obnitram.github.io/sorbonne-calendar/m1/res/ares/m1-res-ares-group4.ics
+
+
 ## Calendrier des cours de M1 SAR
 
 ### NOYAU
@@ -16,38 +26,6 @@ https://obnitram.github.io/sorbonne-calendar/m1/sar/pscr/m1-sar-pscr-group2.ics
 https://obnitram.github.io/sorbonne-calendar/m1/sar/pscr/m1-sar-pscr-group3.ics
 
 
-## Calendrier des cours de M1 STL
-
-### DLP
-https://obnitram.github.io/sorbonne-calendar/m1/stl/dlp/m1-stl-dlp-group1.ics
-
-https://obnitram.github.io/sorbonne-calendar/m1/stl/dlp/m1-stl-dlp-group2.ics
-
-
-### ALGAV
-https://obnitram.github.io/sorbonne-calendar/m1/stl/algav/m1-stl-algav-group1.ics
-
-https://obnitram.github.io/sorbonne-calendar/m1/stl/algav/m1-stl-algav-group2.ics
-
-
-## Calendrier des cours de M1 RES
-
-### ARES
-https://obnitram.github.io/sorbonne-calendar/m1/res/ares/m1-res-ares-group1.ics
-
-https://obnitram.github.io/sorbonne-calendar/m1/res/ares/m1-res-ares-group3.ics
-
-https://obnitram.github.io/sorbonne-calendar/m1/res/ares/m1-res-ares-group4.ics
-
-
-## Calendrier des cours de M1 SFPN
-
-### SC
-https://obnitram.github.io/sorbonne-calendar/m1/sfpn/sc/m1-sfpn-sc-group1.ics
-
-https://obnitram.github.io/sorbonne-calendar/m1/sfpn/sc/m1-sfpn-sc-group2.ics
-
-
 ## Calendrier des cours de M1 SESI
 
 ### ARCHI
@@ -58,5 +36,27 @@ https://obnitram.github.io/sorbonne-calendar/m1/sesi/archi/m1-sesi-archi-group2.
 
 ### VLSI
 https://obnitram.github.io/sorbonne-calendar/m1/sesi/vlsi/m1-sesi-vlsi-group1.ics
+
+
+## Calendrier des cours de M1 SFPN
+
+### SC
+https://obnitram.github.io/sorbonne-calendar/m1/sfpn/sc/m1-sfpn-sc-group1.ics
+
+https://obnitram.github.io/sorbonne-calendar/m1/sfpn/sc/m1-sfpn-sc-group2.ics
+
+
+## Calendrier des cours de M1 STL
+
+### ALGAV
+https://obnitram.github.io/sorbonne-calendar/m1/stl/algav/m1-stl-algav-group1.ics
+
+https://obnitram.github.io/sorbonne-calendar/m1/stl/algav/m1-stl-algav-group2.ics
+
+
+### DLP
+https://obnitram.github.io/sorbonne-calendar/m1/stl/dlp/m1-stl-dlp-group1.ics
+
+https://obnitram.github.io/sorbonne-calendar/m1/stl/dlp/m1-stl-dlp-group2.ics
 
 
--- main.py
+++ main.py
@@ -68,32 +68,6 @@ def main() -> None:
     paths = save_calendars(get_pscr_calendars(filtered_calendars["PSCR"]), "m1/sar/pscr")
     write_links_to_file(paths, link_file, host, "PSCR")
 
-    # M1 STL
-    write_string_to_file(f"## Calendrier des cours de M1 STL\n\n", link_file)
-
-    calendar = get_filtered_calendars_from_url('https://cal.ufr-info-p6.jussieu.fr/caldav.php/STL/M1_STL')
-    filters = ["DLP", "ALGAV"]
-    filtered_calendars: Dict[str, Calendar] = filter_events_by_name(calendar, filters)
-
-    paths = save_calendars(get_dlp_calendars(filtered_calendars["DLP"]), "m1/stl/dlp")
-    write_links_to_file(paths, link_file, host, "DLP")
-
-    paths = save_calendars(get_algav_calendars(filtered_calendars["ALGAV"]), "m1/stl/algav")
-    write_links_to_file(paths, link_file, host, "ALGAV")
-
-
-    # M1 SFPN
-    write_string_to_file(f"## Calendrier des cours de M1 SFPN\n\n", link_file)
-
-    calendar = get_filtered_calendars_from_url('https://cal.ufr-info-p6.jussieu.fr/caldav.php/SFPN/M1_SFPN')
-    filters = ["SC"]
-    filtered_calendars: Dict[str, Calendar] = filter_events_by_name(calendar, filters)
-
-    paths = save_calendars(get_sc_calendars(filtered_calendars["SC"]), "m1/sfpn/sc")
-    write_links_to_file(paths, link_file, host, "SC")
-
-    shutil.copy(link_file, 'public/' + link_file)
-
     # M1 SESI
     write_string_to_file(f"## Calendrier des cours de M1 SESI\n\n", link_file)
 
@@ -106,6 +80,32 @@ def main() -> None:
 
     paths = save_calendars(get_vlsi_calendars(filtered_calendars["VLSI"]), "m1/sesi/vlsi")
     write_links_to_file(paths, link_file, host, "VLSI")
+
+    # M1 SFPN
+    write_string_to_file(f"## Calendrier des cours de M1 SFPN\n\n", link_file)
+
+    calendar = get_filtered_calendars_from_url('https://cal.ufr-info-p6.jussieu.fr/caldav.php/SFPN/M1_SFPN')
+    filters = ["SC"]
+    filtered_calendars: Dict[str, Calendar] = filter_events_by_name(calendar, filters)
+
+    paths = save_calendars(get_sc_calendars(filtered_calendars["SC"]), "m1/sfpn/sc")
+    write_links_to_file(paths, link_file, host, "SC")
+
+    # M1 STL
+    write_string_to_file(f"## Calendrier des cours de M1 STL\n\n", link_file)
+
+    calendar = get_filtered_calendars_from_url('https://cal.ufr-info-p6.jussieu.fr/caldav.php/STL/M1_STL')
+    filters = ["ALGAV", "DLP"]
+    filtered_calendars: Dict[str, Calendar] = filter_events_by_name(calendar, filters)
+
+    paths = save_calendars(get_algav_calendars(filtered_calendars["ALGAV"]), "m1/stl/algav")
+    write_links_to_file(paths, link_file, host, "ALGAV")
+
+    paths = save_calendars(get_dlp_calendars(filtered_calendars["DLP"]), "m1/stl/dlp")
+    write_links_to_file(paths, link_file, host, "DLP")
+
+    shutil.copy(link_file, 'public/' + link_file)
+
 
 if __name__ == '__main__':
     main()
