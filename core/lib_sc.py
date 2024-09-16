15f46f38b6552a1b85051b708c12ee04b825a5fe
ObNitram <mart1.guillemot@gmail.com>
2024-09-16 19:01:08 +0200

    fix Secure coding calendar

--- core/lib_sc.py
+++ core/lib_sc.py
@@ -8,22 +8,21 @@ from core.lib import (
 
 
 def get_sc_calendars(raw_calendar: Calendar) -> Dict[str, Calendar]:
-    noyau_filters = ["Cours", "TME1", "TME2"]
-    noyau_filtered_calendars = filter_events_by_name(raw_calendar, noyau_filters)
+    class_type_filters = ["Cours", "TME 1", "TME 2"]
+    filtered_calendars = filter_events_by_name(raw_calendar, class_type_filters)
 
     group1 = merge_calendars([
-        noyau_filtered_calendars["Cours"],
-        noyau_filtered_calendars["TME1"],
-        noyau_filtered_calendars["default"]
+        filtered_calendars["Cours"],
+        filtered_calendars["TME 1"],
+        filtered_calendars["default"]
     ])
 
     group2 = merge_calendars([
-        noyau_filtered_calendars["Cours"],
-        noyau_filtered_calendars["TME2"],
-        noyau_filtered_calendars["default"]
+        filtered_calendars["Cours"],
+        filtered_calendars["TME 2"],
+        filtered_calendars["default"]
     ])
 
-    # Save each filtered calendar to a separate ICS file
     return {
         "group1": group1,
         "group2": group2,
