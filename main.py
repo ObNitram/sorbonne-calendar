5ff845a9f0f1c2dee7438a31805abc9870d2a79d
bobbywan-kenoby <leonoixdecoco@gmail.com>
2025-01-11 21:09:32 +0100

    add all SAR master courses for 2nd semester (except english and conference)

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ from core.lib_pscr import get_pscr_calendars
 from core.lib_sc import get_sc_calendars
 from core.lib_vlsi import get_vlsi_calendars
 from core.lib_mlbda import get_mlbda_calendars
+from core.lib_pnl import get_pnl_calendars
+from core.lib_ar import get_ar_calendars
+from core.lib_srcs import get_srcs_calendars
+from core.lib_sftr import get_sftr_calendars
+from core.lib_sas import get_sas_calendars
+from core.lib_anglais_sar import get_anglais_sar_calendars
 
 # Replace 'username' and 'password' with your correct credentials
 username = 'student.master'
@@ -33,13 +39,15 @@ host = 'https://obnitram.github.io/sorbonne-calendar/'
 link_file = 'link.md'
 
 start_date = datetime(2024, 9, 1, tzinfo=timezone.utc)
-end_date = datetime(2025, 1, 31, tzinfo=timezone.utc)
+end_date = datetime(2025, 6, 30, tzinfo=timezone.utc)
 
 
 def get_filtered_calendars_from_url(url: str) -> Calendar:
     raw_calendar = load_calendar_from_url(url, auth)
     return filter_events_by_date_range(raw_calendar, start_date, end_date)
 
+
+# course_type_filters = [ "Cours", "cours", "TD1", "TD2", "TD3", "TD", "TME1", "TME2", "TME3", "TME", "TP", "anglais", "Anglais", "ANGLAIS"]
 
 # Main logic
 def main() -> None:
@@ -60,7 +68,7 @@ def main() -> None:
     write_string_to_file(f"## Calendrier des cours de M1 SAR\n\n", link_file)
 
     calendar = get_filtered_calendars_from_url('https://cal.ufr-info-p6.jussieu.fr/caldav.php/SAR/M1_SAR')
-    filters = ["NOYAU", "PSCR"]
+    filters = ["NOYAU", "PSCR", "PNL", "AR", "SRCS", "SFTR", "SAS", "ANGLAIS"]
     filtered_calendars: Dict[str, Calendar] = filter_events_by_name(calendar, filters)
 
     paths = save_calendars(get_noyau_calendars(filtered_calendars["NOYAU"]), "m1/sar/noyau")
@@ -68,6 +76,27 @@ def main() -> None:
 
     paths = save_calendars(get_pscr_calendars(filtered_calendars["PSCR"]), "m1/sar/pscr")
     write_links_to_file(paths, link_file, host, "PSCR")
+
+    # M1 SAR 2nd semestre
+    write_string_to_file(f"## Calendrier des cours de M1 SAR 2nd semestre\n\n", link_file)
+
+    paths = save_calendars(get_pnl_calendars(filtered_calendars["PNL"]), "m1/sar/pnl")
+    write_links_to_file(paths, link_file, host, "PNL")
+    
+    paths = save_calendars(get_ar_calendars(filtered_calendars["AR"]), "m1/sar/ar")
+    write_links_to_file(paths, link_file, host, "AR")
+
+    paths = save_calendars(get_srcs_calendars(filtered_calendars["SRCS"]), "m1/sar/srcs")
+    write_links_to_file(paths, link_file, host, "SRCS")
+
+    paths = save_calendars(get_sftr_calendars(filtered_calendars["SFTR"]), "m1/sar/sftr")
+    write_links_to_file(paths, link_file, host, "SFTR")
+
+    paths = save_calendars(get_sas_calendars(filtered_calendars["SAS"]), "m1/sar/sas")
+    write_links_to_file(paths, link_file, host, "SAS")
+
+    # paths = save_calendars(get_anglais_sar_calendars(filtered_calendars["ANGLAIS"]), "m1/sar/anglais")
+    # write_links_to_file(paths, link_file, host, "ANGLAIS")
 
     # M1 SESI
     write_string_to_file(f"## Calendrier des cours de M1 SESI\n\n", link_file)
